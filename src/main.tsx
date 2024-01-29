8a5c4149039e799bf8c04a45e4e5969d18a870b5
Magnus <magnusrk90@gmail.com>
2024-01-29 14:07:30 +0100

    “[init]”

---
+++ src/main.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import AlbumPicker from './App.tsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    
+	<AlbumPicker />
+  </React.StrictMode>,
+)
